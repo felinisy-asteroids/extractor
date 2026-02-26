b57e387fce938d14a42d7023a8fcb7a3c4a580b0
FeliniSy <ninikheladze9@gmail.com>
2026-02-26 19:15:43 +0400

    change the name for function and url in setting

--- utils/helper.py
+++ utils/helper.py
@@ -3,7 +3,7 @@ from google.cloud import secretmanager
 from utils.settings import settings
 
 
-def access_to_api(project_id: str, secret_name: str):
+def get_secret_value(project_id: str, secret_name: str):
     client = secretmanager.SecretManagerServiceClient()
 
     name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
@@ -14,9 +14,4 @@ def access_to_api(project_id: str, secret_name: str):
 
 
 def get_api_key():
-    return access_to_api(settings.PROJECT_ID, "API_KEY")
-
-
-def get_url():
-    return f"{settings.URL}" + "start_date={0}&end_date={1}&api_key={2}"
-
+    return get_secret_value(settings.PROJECT_ID, "API_KEY")
--- utils/settings.py
+++ utils/settings.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    URL: str = "https://api.nasa.gov/neo/rest/v1/feed?"
+    URL: str = "https://api.nasa.gov/neo/rest/v1/feed?start_date={0}&end_date={1}&api_key={2}"
     PROJECT_ID: str = "cloud-project-workflow"
-    BUCKET_NAME:str = "asteroids-etl"
+    BUCKET_NAME: str = "asteroids-etl"
 
     model_config = SettingsConfigDict(env_file="../.env")
 
